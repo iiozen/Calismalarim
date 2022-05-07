5842a2972732d1433f900778e601d9cadf028b16
Isiner Ismail Ozen <ozen1302@gmail.com>
2022-05-07 14:09:29 +0300

    düzenlendi

--- Calismalarim/BTKPython/Ders22/connection.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-connection = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password = 'MySql1594',
-    database = 'schooldb'
-)
--- Calismalarim/BTKPython/Ders26/catalog/catalog/deneme.py
+++
@@ -1,4 +0,0 @@
-fruits = ["apple", "banana", "cherry"] 
-for x in fruits: 
-    if x == "apple": break 
-    print(x)
